251c932a6245a250ffa1724200720c22df6ea421
ZiN <dima2002302@gmail.com>
2025-03-20 20:11:35 +0300

    gg

--- resources/js/Pages/Entry.jsx
+++ resources/js/Pages/Entry.jsx
@@ -6,7 +6,7 @@ export default function Entry()
 {
     console.log(route('init'));
     return <>
-    <div className="bg-blue-800 w-100 flex h-screen">
+    <div className="bg-blue-900 w-100 flex h-screen">
        init
         </div>
     </>
