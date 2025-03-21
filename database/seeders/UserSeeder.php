48763fe8ed5e67a2852be67434d29c549258ee71
knyazevseven@gmail.com <knyazevseven@gmail.com>
2025-03-21 23:23:10 +0300

    gg

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Gabe',
+            'email' => 'chromakopia@og.com',
+            'password' => Hash::make('123'),
+        ]);
+        User::factory()->count(30)->create();
     }
 }
